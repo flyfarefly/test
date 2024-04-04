9dfbaf9bfd0b57fbb69e34b3768f8ed4d7e1e162
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-04-05 01:13:43 +0300

    HW_33 Practice npm

---
+++ js/index.js
@@ -0,0 +1,17 @@
+`use strict`;
+const sum = function (a, b) {
+  return a + b
+}
+console.log(sum(3, 4))
+
+const mult = function (a, b) {
+  return a * b
+}
+
+console.log(mult(3, 4))
+
+const div = function (a, b) {
+  return a / b
+}
+
+console.log(div(3, 4))
